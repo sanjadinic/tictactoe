71f0320ae0ebe07d4b4a14b2971475573f274b4d
sanjadjak <sanya.dinic@gmail.com>
2017-12-16 16:33:37 +0100

    js

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,26 +1,29 @@
 $(document).ready(function() {
-            var cells;
+    var cells;
 
-            function refresh() {
-                $("#board").remove("table");
-                var table = $("<table>")
-                var tr;
-                $.each(cells, function(i) {
-                    if (!(i % 3) || !tr) {
-                        tr = $("<tr>");
-                    };
-                    var td = $("<td>");
-                    td.attr("id", "cell_" + i);
-                    td.html(cells[i]);
-                    td.click(function() {
-                        $("#result").text("You've clicked on cell number " + $(this).attr("id").split("_")[1] + ".");
-                    })
-                    table.append(tr.append(td));
-                });
-                $("#playground").append(table);
-            }
 
-            function start() {
-                cells = ["", "", "", "", "", "", "", "", ""];
-                refresh();
-            }
+    function refresh() {
+        $("#playground").remove("table");
+        var table = $("<table>")
+        var tr;
+        $.each(cells, function(i) {
+            if (!(i % 3) || !tr) {
+                tr = $("<tr>");
+            };
+            var td = $("<td>");
+            td.attr("id", "cell_" + i);
+            td.html(cells[i]);
+            td.click(function() {
+                $("#result").text("You've clicked on cell number " + $(this).attr("id").split("_")[1] + ".");
+            })
+            table.append(tr.append(td));
+        });
+        $("#playground").append(table);
+    }
+
+    $("#start").click(function() {
+        cells = ["", "", "", "", "", "", "", "", ""];
+        refresh();
+    })
+
+})
