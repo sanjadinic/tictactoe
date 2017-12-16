c7b8622a62272b1c755b84cc4bfe490d2cb1fe59
sanjadjak <sanya.dinic@gmail.com>
2017-12-16 14:56:33 +0100

    changed js code

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,1 +1,26 @@
-var cells = ["", "", "", "", "", "", "", "", ""];
+$(document).ready(function() {
+            var cells;
+
+            function refresh() {
+                $("#board").remove("table");
+                var table = $("<table>")
+                var tr;
+                $.each(cells, function(i) {
+                    if (!(i % 3) || !tr) {
+                        tr = $("<tr>");
+                    };
+                    var td = $("<td>");
+                    td.attr("id", "cell_" + i);
+                    td.html(cells[i]);
+                    td.click(function() {
+                        $("#result").text("You've clicked on cell number " + $(this).attr("id").split("_")[1] + ".");
+                    })
+                    table.append(tr.append(td));
+                });
+                $("#playground").append(table);
+            }
+
+            function start() {
+                cells = ["", "", "", "", "", "", "", "", ""];
+                refresh();
+            }
