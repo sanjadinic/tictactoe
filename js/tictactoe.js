49a1dfb374f6819d54bf2916b9e921c9bc81695e
sanjadjak <sanya.dinic@gmail.com>
2017-12-19 21:02:43 +0100

    changed js

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,29 +1,41 @@
-$(document).ready(function() {
-    var cells;
+  $(document).ready(function() {
+      var cells;
+      var moves = [];
 
+      function refresh() {
+          $("#playground").empty();
+          var table = $("<table>")
+          var tr;
+          $.each(cells, function(i) {
+              if (!(i % 3) || !tr) {
+                  tr = $("<tr>");
+              };
+              var td = $("<td>");
+              td.attr("id", "cell_" + i);
+              td.html(cells[i]);
+              td.click(function() {
+                  $("#result").text("You've clicked on cell number " + $(this).attr("id").split("_")[1] + ".");
+                  if (cells[$(this).attr("id")]) {
+                      return;
+                  }
+                  if (moves.length % 2) {
+                      $("#" + $(this).attr("id")).attr("class", "x");
+                      moves.push("x");
+                      cells[$(this).attr("id")] = "x";
+                  } else {
+                      $("#" + $(this).attr("id")).attr("class", "o");
+                      moves.push("o");
+                      cells[$(this).attr("id")] = "o";
+                  }
+              })
+              table.append(tr.append(td));
+          });
+          $("#playground").append(table);
+      }
 
-    function refresh() {
-        $("#playground").remove("table");
-        var table = $("<table>")
-        var tr;
-        $.each(cells, function(i) {
-            if (!(i % 3) || !tr) {
-                tr = $("<tr>");
-            };
-            var td = $("<td>");
-            td.attr("id", "cell_" + i);
-            td.html(cells[i]);
-            td.click(function() {
-                $("#result").text("You've clicked on cell number " + $(this).attr("id").split("_")[1] + ".");
-            })
-            table.append(tr.append(td));
-        });
-        $("#playground").append(table);
-    }
+      $("#start").click(function() {
+          cells = ["", "", "", "", "", "", "", "", ""];
+          refresh();
+      })
 
-    $("#start").click(function() {
-        cells = ["", "", "", "", "", "", "", "", ""];
-        refresh();
-    })
-
-})
+  })
